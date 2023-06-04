3221ce958ba1fb6e1c466f0df97bfc7a915c2ea3
azlanajju <muhammedazlan11@gmail.com>
2023-06-04 14:03:56 +0530

    added old

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,3 @@
-
 importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js");
 importScripts(
     "https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js",
@@ -11,18 +10,15 @@ importScripts(
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-
-        apiKey: "AIzaSyCAFONpVRASRlZPeCVqekIj1OXvhXtFOEQ",
-        authDomain: "fir-api-c0d9d.firebaseapp.com",
-        databaseURL: "https://fir-api-c0d9d-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "fir-api-c0d9d",
-        storageBucket: "fir-api-c0d9d.appspot.com",
-        messagingSenderId: "911248093259",
-        appId: "1:911248093259:web:794df2cb81aa7b5a0e1dce",
-        measurementId: "G-6ZR5R12NB3"
-    
+    apiKey: "your-api-key",
+    authDomain: "your-auth-domain",
+    databaseURL: "your-database-url",
+    projectId: "your-project-id",
+    storageBucket: "your-storage-bucket",
+    messagingSenderId: "your-sender-id",
+    appId: "your-app-id",
+    measurementId: "your-measurement-id"
 });
-
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
@@ -35,7 +31,7 @@ messaging.setBackgroundMessageHandler(function(payload) {
     );
   
     // Extract the notification message from the payload
-    const notificationData = payload.data;
+    const notificationData = payload.data.notification;
     const notificationTitle = notificationData.title;
     const notificationBody = notificationData.body;
     const notificationIcon = notificationData.icon;
